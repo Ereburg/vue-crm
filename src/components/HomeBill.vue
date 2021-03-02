125083ee30e3fc98277ed5912c310493089cee93
Eugene <eugene.kotsarev@gmail.com>
2021-03-02 23:28:06 +0300

    upload files from last lesson

---
+++ src/components/HomeBill.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="col s12 m6 l4">
+    <div class="card light-blue bill-card">
+      <div class="card-content white-text">
+        <span class="card-title">{{'BillInCurrency'|localize}}</span>
+
+        <p class="currency-line" v-for="cur of currencies" :key="cur">
+          <span>{{getCurrency(cur) | currency(cur)}}</span>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import currencyFilter from "@/filters/currency.filter";
+export default {
+  props: ['rates'],
+  data: () => ({
+    currencies: ['UAH', 'USD', 'EUR']
+  }),
+  computed: {
+    // вычисляем сколько денег на счету в долларах США
+    base() {
+      return this.$store.getters.info.bill / this.rates['UAH']
+    }
+  },
+  methods: {
+    // сколько у нас денег в разных валютах (currency)
+    getCurrency(currency) {
+      console.log(' HomeBill.vue - rates: ', this.rates)
+      return Math.floor(this.base * this.rates[currency] * 100) / 100
+    }
+  }
+}
+</script>
